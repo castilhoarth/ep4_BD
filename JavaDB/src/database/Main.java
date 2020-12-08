18a16c0cbc1d7f69696e56fb75eab88cba244196
Arthur Castilho <castartx@gmail.com>
2020-12-08 09:45:42 -0300

    Classe de conexao

---
+++ JavaDB/src/database/Main.java
@@ -0,0 +1,30 @@
+package database;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		getConnection();
+	}
+
+	public static Connection getConnection() throws Exception {
+		try {
+			String driver = "com.mysql.cj.jdbc.Driver";
+			String url = "jdbc:mysql://127.0.0.1/aula?useTimezone=true&serverTimezone=UTC";
+			String username = "root";
+			String password = "";
+			Class.forName(driver);
+			
+			Connection conn = DriverManager.getConnection(url,username,password);
+			System.out.println("Connected");
+			return conn;
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		return null ;
+	}
+}
